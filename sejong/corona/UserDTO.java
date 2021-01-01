8b5b181619b86cc2e0504019fd35bb20acfdda7e
yunbeom <uiurihappy@sju.ac.kr>
2021-01-01 22:19:59 +0900

    UserDTO 파일 수정
    제가 ManagerDTO에서 사용했던 UserDTO 파일 업데이트

--- sejong/corona/UserDTO.java
+++ sejong/corona/UserDTO.java
@@ -1,103 +1,128 @@
-package sejong.corona;
 
 public class UserDTO {
-	private String name;
-	private String phone;
-	private String address;
-	private String birth;
-	private String gender;
-	private String symptom;
-	private String etc;
-	private String hospital;
-	private String date;
-	private String status;
-	private String result;
-
-	public String getName() {
-		return name;
+	private int id;
+    private String name;
+    private String phone;
+    private String address;
+    private String birth;
+    private String gender;
+    private String Symptom1;
+    private String Symptom2;
+    private String Symptom3;
+    private String Symptom4;
+    private String etc;
+    private String hospital;
+    private String _date;
+    private String _status;
+    private String result;
+    
+    
+    public int getId() {
+    	return id;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+    public void setId(int prid) {
+        this.id = id;
+    }
+    
+    public String getName() {
+    	return name;
 	}
-
-	public String getPhone() {
-		return phone;
+    public void setName(String name) {
+        this.name = name;
+    }
+   
+    public String getPhone() {
+    	return phone;
 	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getAddress() {
+    	return address;
 	}
-
-	public String getAddress() {
-		return address;
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getBirth() {
+    	return birth;
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+    
+    public String getGender() {
+    	return gender;
 	}
-
-	public String getBirth() {
-		return birth;
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public String getSymptom1() {
+    	return Symptom1;
 	}
-
-	public void setBirth(String birth) {
-		this.birth = birth;
+    public void setSymptom1(String Symptom1) {
+        this.Symptom1 = Symptom1;
+    }
+    
+    public String getSymptom2() {
+    	return Symptom2;
 	}
-
-	public String getGender() {
-		return gender;
+    public void setSymptom2(String Symptom2) {
+        this.Symptom2 = Symptom2;
+    }
+    
+    public String getSymptom3() {
+    	return Symptom3;
 	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
+    public void setSymptom3(String Symptom3) {
+        this.Symptom3 = Symptom3;
+    }
+    
+    public String getSymptom4() {
+    	return Symptom4;
 	}
-
-	public String getSymptom() {
-		return symptom;
+    public void setSymptom4(String Symptom4) {
+        this.Symptom4 = Symptom4;
+    }
+    
+    public String getEtc() {
+    	return etc;
 	}
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
 
-	public void setSymptom(String symptom) {
-		this.symptom = symptom;
+    public String getHospital() {
+    	return hospital;
 	}
-
-	public String getEtc() {
-		return etc;
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+    
+    public String getDate() {
+    	return _date;
 	}
-
-	public void setEtc(String etc) {
-		this.etc = etc;
+    public void setDate(String _date) {
+        this._date = _date;
+    }
+    
+    public String getStatus() {
+    	return _status;
 	}
-
-	public String getHospital() {
-		return hospital;
+    public void setStatus(String _status) {
+        this._status = _status;
+    }
+    
+    public String getResult() {
+    	return result;
 	}
-
-	public void setHospital(String hospital) {
-		this.hospital = hospital;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getResult() {
-		return result;
-	}
-
-	public void setResult(String result) {
-		this.result = result;
-	}
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+    
+    
+    
 }
